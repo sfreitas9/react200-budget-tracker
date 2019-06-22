18a58976b2a2d47e2c0dddfaaf20c38623368e63
Sherry Freitas <sherry.freitas@gmail.com>
2019-06-21 20:00:07 -0700

    adds reducers

---
+++ src/js/components/ExpenseEntries/expenseReducer.js
@@ -0,0 +1,10 @@
+const defaultState = {
+  description: '',
+  amount: '',
+  lineItems: []
+};
+
+export default function ExpenseReducer (state = defaultState, action) {
+
+  return state;
+}
---
+++ src/js/components/IncomeEntries/incomeReducer.js
@@ -0,0 +1,10 @@
+const defaultState = {
+  description: '',
+  amount: '',
+  lineItems: []
+};
+
+export default function IncomeReducer (state = defaultState, action) {
+  
+  return state;
+}
