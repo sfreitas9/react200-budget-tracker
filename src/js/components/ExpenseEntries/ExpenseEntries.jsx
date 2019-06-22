b6567465194c059991e38f95d03b02d8bd623bd1
Sherry Freitas <sherry.freitas@gmail.com>
2019-06-21 21:10:26 -0700

    implements store

--- src/js/components/ExpenseEntries/ExpenseEntries.jsx
+++ src/js/components/ExpenseEntries/ExpenseEntries.jsx
@@ -1,17 +1,51 @@
 /* eslint-disable no-useless-constructor */
 import React from 'react';
+import PropTypes from 'prop-types';
+
+// We'll need to import all those action creators.
+import {
+  updateExpenseDescription,
+  updateExpenseAmount,
+  addExpense
+} from './expenseActions';
 
 export default class ExpenseEntries extends React.Component {
   constructor(props) {
     super(props);
+
+    // Here we're binding these methods to the context
+    // of the components. This only has to be done,
+    // because these methods are called back by
+    // event emitters (which lose context).
+    this.handleDescriptionInput = this.handleDescriptionInput.bind(this);
+    this.handleAmountInput = this.handleAmountInput.bind(this);
+    this.handleAddExpense = this.handleAddExpense.bind(this);
+  }
+
+  handleDescriptionInput(event) {
+    // dispatch was provided by connect()
+    const { dispatch } = this.props;
+    const { value } = event.target;
+    dispatch(updateExpenseDescription(value));
+  }
+
+  handleAmountInput(event) {
+    const { dispatch } = this.props;
+    const { value } = event.target;
+    dispatch(updateExpenseAmount(value));
+  }
+
+  handleAddExpense() {
+    const { description, amount, dispatch } = this.props;
+    dispatch(addExpense(description, amount));
   }
 
   render() {
+    // These values were provided by connect()
+    const { description, amount, lineItems } = this.props;
     return (
       <div className='card border-danger mb-3'>
-        <div className='card-header text-white bg-danger'>
-          Expense Entries
-        </div>
+        <div className='card-header text-white bg-danger'>Expense Entries</div>
         <div className='card-body'>
           <form>
             <div className='form-group'>
@@ -20,6 +54,8 @@ export default class ExpenseEntries extends React.Component {
                 type='text'
                 className='form-control'
                 id='expense-description'
+                value={ description }
+                onChange={ this.handleDescriptionInput }
               />
             </div>
             <div className='form-group'>
@@ -30,12 +66,15 @@ export default class ExpenseEntries extends React.Component {
                   type='text'
                   className='form-control'
                   id='expense-amount'
+                  value={ amount }
+                  onChange={ this.handleAmountInput }
                 />
               </div>
             </div>
             <button
               type='button'
               className='btn btn-danger col-12 mb-5'
+              onClick={ this.handleAddExpense }
             >+ Add Expense
             </button>
             <table className='table table-sm table-hover'>
@@ -46,10 +85,14 @@ export default class ExpenseEntries extends React.Component {
                 </tr>
               </thead>
               <tbody>
-                <tr>
-                  <td>Rent</td>
-                  <td>$1,500.00</td>
-                </tr>
+                {
+                  lineItems.map(lineItem => (
+                    <tr>
+                      <td>{ lineItem.description }</td>
+                      <td>${ lineItem.amount.toFixed(2) }</td>
+                    </tr>
+                  ))
+                }
               </tbody>
             </table>
           </form>
@@ -58,3 +101,9 @@ export default class ExpenseEntries extends React.Component {
     );
   }
 }
+
+ExpenseEntries.propTypes = {
+  description: PropTypes.string,
+  amount: PropTypes.number,
+  lineItems: PropTypes.array
+};
--- src/js/components/ExpenseEntries/expenseReducer.js
+++ src/js/components/ExpenseEntries/expenseReducer.js
@@ -4,7 +4,45 @@ const defaultState = {
   lineItems: []
 };
 
-export default function ExpenseReducer (state = defaultState, action) {
+export default function ExpenseReducer(state = defaultState, action) {
+  const { type, payload } = action;
 
-  return state;
+  switch (type) {
+    // Here in the case of the update description action 
+    case 'UPDATE_EXPENSE_DESCRIPTION': {
+      // we'll return an object
+      return {
+        // with all the previous state
+        ...state,
+        // but overwriting the description
+        description: payload.description
+      };
+    }
+
+    case 'UPDATE_EXPENSE_AMOUNT': {
+      return {
+        ...state,
+        amount: payload.amount
+      };
+    }
+
+    case 'ADD_EXPENSE': {
+      const { description, amount } = action.payload;
+      return {
+        description: '',
+        action: '',
+        lineItems: [
+          // here we have all the previous line items
+          ...state.lineItems,
+          // plus a new object
+          { description, amount }
+        ]
+      };
+    }
+
+    default: {
+      return state;
+    }
+  }
 }
+
